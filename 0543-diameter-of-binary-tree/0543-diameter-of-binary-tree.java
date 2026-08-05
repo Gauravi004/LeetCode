070050703619796a782e2d5796762da97e62a815
Gauravi Mungia <gauravi4790.se24@chitkara.edu.in>
2026-08-05 19:24:51 +0530

    Time: 1 ms (45.66%), Space: 47 MB (74.52%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,31 +14,23 @@
  * }
  */
 class Solution {
+           int ans =0;
     public int diameterOfBinaryTree(TreeNode root) {
-        int max=Integer.MIN_VALUE;
-        if(root==null){
-            return 0;
-        }
-
-       int lefdia = diameterOfBinaryTree(root.left);
-       int rightdia = diameterOfBinaryTree(root.right); 
-       int cur_dia = height(root.left) + height(root.right);
-
-    //    max =Math.max(lefdia, rightdia);
-
-       return Math.max(cur_dia, Math.max(lefdia, rightdia));
+      height(root);
+      return ans;
 
 
     }
 
     public int height(TreeNode root){
+ 
         if(root == null){
             return 0;
         }
 
         int left = height(root.left);
         int right = height(root.right);
-
+        ans = Math.max(ans,  left+ right);
         return Math.max(left , right) + 1;
     }
 }
