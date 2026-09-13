488cf62d8463fde3b4617eb251d7d80aa3ae0b1b
Gauravi Mungia <gauravi4790.se24@chitkara.edu.in>
2026-09-13 17:55:21 +0530

    Time: 14 ms (69.95%), Space: 47.6 MB (66.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,29 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        for(int num:nums){
-            mp.put(num, mp.getOrDefault(num,0) + 1 );
-        }
-
-        PriorityQueue<Pair<Integer, Integer>> MinHeap =  new PriorityQueue<>((a, b) -> a.getKey() - b.getKey());
-        ArrayList<Integer> result = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-            MinHeap.add(new Pair<>(entry.getValue(), entry.getKey()));
-
-            if(MinHeap.size() > k){
-                MinHeap.poll();
-
-            }
-        }
-
-        while(!MinHeap.isEmpty()){
-            result.add(MinHeap.poll().getValue());
-
-
-        }
-        int[] ans = new int[k];
-for (int i = k - 1; i >= 0; i--) {
-    ans[i] = result.get(i);
-}
-return ans;
-
-        
+   public int[] topKFrequent(int[] nums, int k) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for(int i=0; i<nums.length; i++){
+        map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
     }
+
+    // min-heap by frequency
+    PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+
+    for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+        pq.add(new int[]{entry.getKey(), entry.getValue()});
+        if(pq.size() > k){
+            pq.poll();
+        }
+    }
+
+    List<Integer> list = new ArrayList<>();
+    while(!pq.isEmpty()){
+        list.add(pq.poll()[0]);
+    }
+
+    int[] ans = new int[k];
+    for(int i=0; i<list.size(); i++){
+        ans[i] = list.get(i);
+    }
+    return ans;
+}
 }
